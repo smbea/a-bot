e8967a8cbc87ea9c122180ccfff0d5aae383e7ac
Beatriz Mendes <bea.smendes98@gmail.com>
2021-12-17 11:00:39 +0000

    Create new issue

--- index.js
+++ index.js
@@ -2,20 +2,83 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
+
+const find = require('min-dash').find;
+const MODERATORS = require('./resources/assignes');
+
 module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+  app.on("issues.closed", async (context) => {
+
+    const {
+      octokit,
+      payload
+    } = context;
+
+    const {
+      issue,
+      repository
+    } = payload;
+
+    if (!hasWeeklyLabel(issue)) {
+      return;
+    }
+
+    const {
+      login,
+      fullName
+    } = getNextRoundRobin(issue, 1)|| {};
+
+    const {
+      data: createdIssue
+    } = await octokit.issues.create({
+      owner: repository.owner.login,
+      repo: repository.name,
+      title: "created by bot",
+      labels: [ 'team event', 'ready' ],
+      assignees: ['smbea']
     });
-    return context.octokit.issues.createComment(issueComment);
+
+    const issueComment = context.issue({
+      body: `Opened next one: ${createdIssue.html_url}.
+      Assigne: ${fullName}`,
+    });
+
+    return octokit.issues.createComment(issueComment);
   });
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
+
+// helper ////////////////
+
+function hasWeeklyLabel(issue) {
+  const {
+    labels
+  } = issue;
+
+  return !!find(labels, (l) => l.name === 'team event');
+}
+
+function getNextRoundRobin(closedIssue, offset = 1) {
+  function transformIntoBounds(idx, length) {
+    return idx >= length ? transformIntoBounds(idx - length, length) : idx;
+  }
+
+  const {
+    assignee
+  } = closedIssue;
+
+  if (!assignee) {
+    return;
+  }
+
+  const lastAssignee = find(MODERATORS, m => m.login === assignee.login);
+
+  // ensure assignee was a valid moderator
+  if (!lastAssignee) {
+    return;
+  }
+
+  return MODERATORS[transformIntoBounds(lastAssignee.idx + offset, MODERATORS.length)];
+}
---
+++ resources/assignes.js
@@ -0,0 +1,5 @@
+module.exports = [
+    { idx: 0, fullName: 'bea', login: 'smbea' },
+    { idx: 1, fullName: '2', login: 'b' },
+    { idx: 2, fullName: '3', login: 'c' },
+  ];
